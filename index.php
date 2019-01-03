34be2df6ae554f5a0fc472e6001e14378638db1b
Muhammad Dienulloh Ulil Abshar <lugiax14@gmail.com>
2019-01-03 17:10:25 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <title>Tugas Pak Bana</title>
+
+    <!-- Bootstrap core CSS -->
+<link href="Boostrap/bootstrap.css" rel="stylesheet">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="Boostrap/album.css" rel="stylesheet">
+  </head>
+  <body>
+<?php include "isi/header.php" ?>
+
+<?php include "isi/body.php" ?>
+
+<?php include "isi/footer.php" ?>
+</body>
+</html>
---
+++ isi/footer.php
@@ -0,0 +1,20 @@
+<footer class="text-muted">
+  <div class="container">
+    <p class="float-right">
+      <a href="#">Back to top</a>
+    </p>
+    <p>&copy; Copyright to <a href="https://www.facebook.com/myhurt.inheart"> Muhammad Dienulloh Ulil A. </a>, interested to edit it? PM</p>
+    <p>Created to Fullfill the Requirement of Project. Lecture Computer and Architechture Assignment..</p>
+  </div>
+</footer>
+
+
+<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script>window.jQuery || document.write('<script src="/docs/4.2/assets/js/vendor/jquery-slim.min.js"><\/script>')</script><script src="/docs/4.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-zDnhMsjVZfS3hiP7oCBRmfjkQC4fzxVxFhBx8Hkz2aZX8gEvA/jsP3eXRCvzTofP" crossorigin="anonymous"></script></body>
+-->
+
+<script src="jquery/jquery-3.1.1.js"></script>
+<script src="Boostrap/bootstrap.bundle.min.js" > </script>
+<script src="Boostrap/bootstrap-theme.min.css" > </script>
+
+
